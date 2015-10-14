80fbb10af1aa291006f27bee60101f51fcdb5cff
Derk van der Heide <derkvanderheide@hotmail.com>
2015-10-14 23:19:55 +0200

    Modified base template and index page for news bundle.

--- src/Autopoets/NewsBundle/Controller/DefaultController.php
+++ src/Autopoets/NewsBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Autopoets\NewsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('NewsBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('NewsBundle:Default:index.html.twig');
     }
 }
